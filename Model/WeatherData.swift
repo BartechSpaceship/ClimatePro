ff9eb6f51d8336fee5d87c24432f0a048ae4bb35
Bartek <bartek.daniel24@gmail.com>
2020-03-03 17:37:22 -0600

    Initial commit

---
+++ Model/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  ClimatePro
+//
+//  Created by Bartek on 3/3/20.
+//  Copyright © 2020 Bartek. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
